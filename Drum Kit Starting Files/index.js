07f91ec6d9917406e0a964ac2d5980883f34443f
newdank <nikhil1vasan@gmail.com>
2023-11-29 18:18:40 +0530

    Update index.js

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,1 +1,11 @@
-alert("welcome");
+// function handleClick(){
+//   alert("The button got clicked");
+// }
+var drumButtons=document.querySelectorAll(".drum").length;
+
+for(var i=0; i<drumButtons;i++){
+
+document.querySelector("button")[i].addEventListener("click",function(){
+  alert("The button is clicked");
+});
+}
